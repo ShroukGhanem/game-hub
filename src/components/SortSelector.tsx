4c085f60c517d6ebc03addc6a69517c16c632434
Shrouk Ghanem <shrouk.a.ghanem@gmail.com>
2024-06-23 20:58:35 +0300

    Sorting games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,11 +1,37 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average rating" },
+  ];
+
+  const currentSortOrder = sortOrders.find((order) => order.value == sortOrder);
+
   return (
     <Menu>
-      <MenuButton as={Button}>Order By</MenuButton>
+      <MenuButton as={Button}>
+        Order By: {currentSortOrder?.label || "Relevance"}
+      </MenuButton>
       <MenuList>
-        <MenuItem>Placeholder</MenuItem>
+        {sortOrders.map((sortOrder) => (
+          <MenuItem
+            key={sortOrder.value}
+            value={sortOrder.value}
+            onClick={() => onSelectSortOrder(sortOrder.value)}
+          >
+            {sortOrder.label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
